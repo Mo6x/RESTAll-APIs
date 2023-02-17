1358850066ef3e41d61257aa3fc69652f1fb7480
mac@Dev <christmoses1999@gmail.com>
2023-02-17 11:23:37 +0100

    install react

--- client-side/my-app/src/App.js
+++ client-side/my-app/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={router}/>
+    </div>
   )
 }
-
 export default App
