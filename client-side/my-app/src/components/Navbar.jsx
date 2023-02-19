67e1388797756500c2344e83843de1aed4f521fb
mac@Dev <christmoses1999@gmail.com>
2023-02-19 19:53:09 +0100

    Designing all the neccessary pages including styling

--- client-side/my-app/src/components/Navbar.jsx
+++ client-side/my-app/src/components/Navbar.jsx
@@ -1,15 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../asserts/logo.jpeg";
 
 
 const Navbar = () => {
     return(
         <div className="navbar">
         <div className="container">
-          <div className="logo">Logo</div>
-          <div className="links">Links</div>
+          <div className="logo">
+            <img src={Logo} alt="" />
+          </div>
+        <div className="links">
+        <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+            </Link>
+            <Link className="link" to="/?cat=art">
+            <h6>SCIENCE</h6>
+            </Link>
+            <Link className="link" to="/?cat=art">
+            <h6>TECHNOLOGY</h6>
+            </Link>
+            <Link className="link" to="/?cat=art">
+            <h6>CINEMA</h6>
+            </Link>
+            <Link className="link" to="/?cat=art">
+            <h6>DESIGN</h6>
+            </Link>
+            <Link className="link" to="/?cat=art">
+            <h6>FOOD</h6>
+            </Link>
         </div>
         </div>
-    )
-}
+        </div>
+    );
+};
 
 export default Navbar;
