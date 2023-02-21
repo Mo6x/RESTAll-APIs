1a31a7cbaed575164279f8e530041dfd7fa376f9
mac@Dev <christmoses1999@gmail.com>
2023-02-21 01:15:48 +0100

    Successfully design the Home page and the styles, I want to start working on the Single page now

--- client-side/my-app/src/pages/Home.jsx
+++ client-side/my-app/src/pages/Home.jsx
@@ -1,33 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import IMG1 from "../asserts/spring-onion.jpeg";
+import IMG2 from "../asserts/glass-cup.jpeg";
+import IMG3 from "../asserts/onion.jpeg";
+import IMG4 from "../asserts/oranges.jpeg";
+ 
 
 const Home = () => {
    
     const posts = [
         {
             id: 1,
-            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit      voluptates tempora ullam amet. Dicta eius voluptatem natus ipsam.", 
-            desc: "Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
-            imgs: ""
+            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit.", 
+            desc: " suscipit voluptates tempora ullam amet  Dicta eius voluptatem natus ipsam, Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
+            img: IMG1
         },
         {
             id: 2,
-            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit      voluptates tempora ullam amet. Dicta eius voluptatem natus ipsam.", 
-            desc: "Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
-            imgs: ""
+            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit.", 
+            desc: " suscipit voluptates tempora ullam amet  Dicta eius voluptatem natus ipsam, Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
+            img: IMG2
         },
         {
             id: 3,
-            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit      voluptates tempora ullam amet. Dicta eius voluptatem natus ipsam.", 
-            desc: "Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
-            imgs: ""
+            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit.", 
+            desc: " suscipit voluptates tempora ullam amet  Dicta eius voluptatem natus ipsam, Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
+            img: IMG3
         },
         {
             id: 4,
-            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit      voluptates tempora ullam amet. Dicta eius voluptatem natus ipsam.", 
-            desc: "Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
-            imgs: ""
+            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil laudantium, suscipit.", 
+            desc: " suscipit voluptates tempora ullam amet  Dicta eius voluptatem natus ipsam, Exercitationem officia ea id asperiores dolorem facere debitis natus! Dolorem.",
+            img: IMG4
         },
     ]
 
@@ -36,8 +40,8 @@ const Home = () => {
            <div className="posts">
                 {posts.map((post) => (
                     <div className="post" key={post.id}>
-                        <div className="imgs">
-                            <img src={post.imgs} alt="" />
+                        <div className="img">
+                            <img src={post.img} alt="" />
                         </div>
                           <div className="content">
                             <Link className="link" to={`/post/${post.id}`}>
