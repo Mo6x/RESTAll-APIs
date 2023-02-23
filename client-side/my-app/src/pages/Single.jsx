76fefc6d61dc7aed7cf553725044f51d2d29c28d
mac@Dev <christmoses1999@gmail.com>
2023-02-23 12:55:30 +0100

    Successfully design the Home page and the styles, I want to start working on the Single page now

--- client-side/my-app/src/pages/Single.jsx
+++ client-side/my-app/src/pages/Single.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Flower from "../asserts/smart-flower.jpeg";
+import Christ from "../asserts/Christ.jpeg";
+
 
 
 const Single = () => {
     return(
-        <div></div>
+        <div className="single">
+            <div className="content">
+                <img src={Flower} alt="" />
+                <div className="user">
+                    <img src={Christ} alt="" />
+                    <div className="info">
+                        <span>Christ</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                </div>
+            </div>
+            <div className="menu">M</div>
+        </div>
     )
 }
 export default Single;
--- client-side/my-app/src/pages/Write.jsx
+++ client-side/my-app/src/pages/Write.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Write = () => {
     return(
         <div>
-
+             
         </div>
     )
 }
