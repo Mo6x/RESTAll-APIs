3948b86c6f0079f3dce1192351185dae41afe220
mac@Dev <christmoses1999@gmail.com>
2023-02-28 21:47:45 +0100

    Start building my routes already and login page

--- server-side/index.js
+++ server-side/index.js
@@ -1,10 +1,17 @@
 import express from "express";
-
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/posts.js";
+import postRoutes from "./routes/users.js";
 
 
 const app = express();
 
 app.use(express.json());
+app.use("api/auth", authRoutes);
+app.use("api/user", userRoutes);
+app.use("api/post", postRoutes);
+
+
 
 app.listen(5000, ()=> {
     console.log("Server Connected Successfully");
--- server-side/routes/posts.js
+++ server-side/routes/posts.js
@@ -1,8 +1,7 @@
-import express, { Router } from "express";
+import express from "express";
+
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json("")
-})
+
 export default router;
