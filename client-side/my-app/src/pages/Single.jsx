813650ccc02ca5d3ab1c8933cad774ee86843fc8
mac@Dev <christmoses1999@gmail.com>
2023-03-06 18:07:22 +0100

    creted the post request from the backend to frontend using contextAPI

--- client-side/my-app/src/pages/Single.jsx
+++ client-side/my-app/src/pages/Single.jsx
@@ -1,36 +1,59 @@
-import React from "react";
-import fruit from "../asserts/tropical-fruits.jpeg";
+import React, { 
+    useState,
+   useLocation,
+   useEffect,
+   useContext,
+} from "react";
+//import fruit from "../asserts/tropical-fruits.jpeg";
 import christ from "../asserts/Christ.jpeg";
 import Edit from "../asserts/edit.png";
 import Delete from "../asserts/delete.jpeg";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 import Menu from "../components/Menu";
-
+import axios from "axios";
+import moment from "react"
+import { AuthContext } from "../components/context/authContext";
 
 
 const Single = () => {
-     
+     const [post, setPosts] = useState({});
+     const location = useLocation()
+     const postId = location.pathname.split("/")[2];
+     const {currentUser} = useContext(AuthContext)
 
+    useEffect(() => {
+        const fetchData = async ()=> {
+            try{
+                const res = await axios.get(`/posts/${postId}`);
+                setPosts(res.data);
+            }catch (err) {
 
+            }
+        };
+        fetchData();
+    }, [postId]); 
 
     return(
         <div className="single">
             <div className="content">
-            <img src={fruit} className="tropical-fruits" alt="fruit" />
+            <img src={post?.img} alt="" />
                 <div className="user">
                 <img src={christ} className="App-logo" alt="logo" />
                     <div className="info">
-                        <span>Susan</span>
-                        <p>Posted 2 days ago</p>
+                        <span>{post.username}</span>
+                        <p>Posted { moment(post.date).fromNow() }</p>
                     </div>
+                 {currentUser.username === post.username && (
                     <div className="edit">
                         <Link to={`/write?edit=2`}>
                        <img src={Edit} alt="edit pics" />
                         </Link>
                        <img src={Delete} alt="delete pic" />
                     </div>
+                )}    
                 </div>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <h1>{post.title}</h1>
+                {post.desc}
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut veniam, animi laborum molestiae ad temporibus ut consequuntur, officia sed quae sit at nesciunt delectus accusamus aliquid, nobis vero fuga deserunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae adipisci eius dolor animi hic, officia dolore enim quo maiores iste! Est aspernatur blanditiis maiores aperiam ea nihil rerum, repellendus.
                     <br />
                     <br />
@@ -48,10 +71,11 @@ const Single = () => {
                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus totam enim, blanditiis voluptatum, suscipit quae architecto aliquid unde accusantium natus hic possimus, ipsum illo eum aliquam molestiae voluptate doloremque iste. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nobis ipsa delectus quos eaque quisquam, reprehenderit doloribus mollitia officia velit inventore ad neque itaque nesciunt error. Sunt voluptatum sapiente fuga.
                       
                     
-                    </p> </p>
+                    </p>
+                     </p>
             </div>
             <Menu />
         </div>
-    )
-}
+    );
+};
 export default Single;
