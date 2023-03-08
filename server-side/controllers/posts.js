8c01beeb7298b74566a0650a6c4ae5c3c4507d5a
mac@Dev <christmoses1999@gmail.com>
2023-03-08 12:57:48 +0100

    Install multer for imagee upload and storege in the client side

--- server-side/controllers/posts.js
+++ server-side/controllers/posts.js
@@ -1,3 +1,50 @@
+import { db } from "../db.js";
+import jwt from "jsonwebtoken";
+
+
+export const getPosts = (req, res) => {
+    const q = req.query.cat ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts"
+
+    db.query(q, [req.query.cat], (err, data) => {
+        if (err) return res.send(err);
+        return res.status(500).json(data);
+    });
+};
+
+
+export const getPost = (req, res) => {
+    const q = "SELECT `username`, `title`, `desc`, p.img, u.img, `cat`, `date`, FROM users u JSON posts p ON u.id=p.uid WHERE p.id = ? "
+
+    db.query(q, [req.params.id], (err, data) => {
+        if(err) return res.json(err);
+        return res.status(200).json(data[0]);
+    });
+};
+
 export const addPost = (req, res) => {
-    res.json("")
-}
+    res.json("from controller")
+};
+
+
+export const deletePost = (req, res) => {
+    const token = req.cookie.access_token
+    if(!token) return res.status(401).json("Not authenticated");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid");
+
+        const postId =  req.params.id
+        const q = "DELETE FROM post WHERE `id` = ? AND `uid` = ?";
+
+        db.query(q, [postId, userInfo.id], (err, data) => {
+            if(err) return res.status(403).json("You can delete only your post");
+            return res.json("post has been deleted");
+        })
+    })
+};
+
+
+export const updatePost = (req, res) => {
+    res.json("from controller")
+};
--- server-side/index.js
+++ server-side/index.js
@@ -3,12 +3,22 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/posts.js";
 import postRoutes from "./routes/users.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("file"), function(req, res){
+    const file = req.file;
+    res.status(200).json("file.filename");
+})
+
 app.use("api/auth", authRoutes);
 app.use("api/user", userRoutes);
 app.use("api/post", postRoutes);
